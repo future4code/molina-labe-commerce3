f90133f4e744b77901a32187a6342a3177d9d678
Lucas Gomes <lucasgomesbr@gmail.com>
2021-06-10 17:51:51 -0300

    Update Main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,10 +2,6 @@
 import React from 'react';
 import Filtros from '../filtro/Filtros';
 import Produtos from '../produtos/Produtos';
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 import { ContainerMain, ContainerProdutos } from './styled'
 
 class Main extends React.Component {
