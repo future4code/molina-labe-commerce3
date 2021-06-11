afe10c4e7551c2f02a517e487199dc16ff8205d6
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-06-10 22:53:47 -0300

    comentarios

--- src/components/filtro/styled.js
+++ src/components/filtro/styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 const ContainerFiltro = styled.div`
-    margin-top: 15vh;
+    margin-top: 15vh; //Deixou o filtro muito pra baixo
     padding: 10px;
     display: flex;
     flex-direction: column;
     margin-bottom: 20vh;
-    cursor: pointer;
+    cursor: pointer; //Geralmente é usado em botoes e coisas q voce clica,m, nao no container inteiro
     background-color: #F3F3F3;
     text-align: left;
 `
