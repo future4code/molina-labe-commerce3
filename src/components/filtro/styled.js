58fcc5e21f772e9abfb5843d602c257a628992ab
Ishmaiha <ishmaihahkim@gmail.com>
2021-06-10 22:26:17 -0300

    segunda parte filtro

--- src/components/filtro/styled.js
+++ src/components/filtro/styled.js
@@ -1,14 +1,29 @@
 import styled from 'styled-components'
 
 const ContainerFiltro = styled.div`
-    margin-top: 15vh;
+    margin-top: 10vh;
     padding: 10px;
     display: flex;
     flex-direction: column;
-    margin-bottom: 20vh;
+    margin-bottom: 15vh;
     cursor: pointer;
     background-color: #F3F3F3;
     text-align: left;
+    height: 400px;
+`
+const ContainerLabel = styled.div`
+    color: #333;
+    padding: 20px 20px 20px 0px;
+    display: grid;
 `
 
-export {ContainerFiltro}
+const Titulo = styled.h3`
+    color: #333;
+`
+
+const Label = styled.label`
+    font-weight: bold;
+    margin-bottom: 8px;
+`
+export {ContainerFiltro, ContainerLabel, Titulo, Label}
+
